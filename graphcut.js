03a9ac652119802a22c98e531ebc5bf4be02f292
Valentin <valentin.giraud@etu.univ-lyon1.fr>
2019-06-11 14:49:55 +0200

    Added option for loading image path

--- graphcut.js
+++ graphcut.js
@@ -33,7 +33,7 @@ function Graphcut(img, apiKey, callback, options) {
 
     this.pLength = options.pLength || 400;
 
-    this.directory = options.directory || "./";
+    this.loadingImgPath = options.loadingImgPath || "loading.gif";
     this.modalTpl = options.modalTpl || "graphcutModal.html";
     this.workerJS = options.workerJS || "worker.js";
 
@@ -381,7 +381,7 @@ Graphcut.prototype.init = function () {
         $("#whiteshop-preview-img").show();
 
         //Need real path
-        $("#whiteshop-preview-img").attr("src",that.directory+"loading.gif");
+        $("#whiteshop-preview-img").attr("src",that.loadingImgPath);
         
         $("#whiteshop-zoom1").click();
         that.worker.postMessage(["export"]);
